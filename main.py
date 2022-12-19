94bcf3d7b06e7a9ced7f07c5e13b585f9c86732f
abenteuerzeit <amroz.muzyka@gmail.com>
2022-12-19 01:39:39 +0100

    refactored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
-from stages import stages
 import os
+from stages import stages as ASCII_ART
 
 difficulty_levels = {
     'easy': 8,
@@ -11,29 +11,6 @@ difficulty_levels = {
 
 
 # Helper functions
-def choose_difficulty():
-    print("Select a difficulty level:")
-    print("1. Easy (8 lives)")
-    print("2. Medium (6 lives)")
-    print("3. Hard (4 lives)")
-    print("4. Expert (2 lives)")
-    print("5. Custom (Enter your own number of lives)")
-    while True:
-        selection = input("Selecte a difficulty level (1-5) or enter your own number of lives: ")
-        if selection == '1':
-            return 'easy'
-        elif selection == '2':
-            return 'medium'
-        elif selection == '3':
-            return 'hard'
-        elif selection == '4':
-            return 'expert'
-        elif selection == '5':
-            print("Enter the number of lives:")
-            return input()
-        print("Invalid selection. Please enter a number between 1 and 5.")
-
-
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -44,7 +21,7 @@ def display_title_screen():
 
 
 def get_game(difficulty):
-    return (get_lives(difficulty), random.choice(get_words()))
+    return (get_lives(difficulty), get_secret(difficulty))
 
 
 def get_lives(difficulty):
@@ -62,6 +39,34 @@ def get_logo():
 def get_words():
     with open('countries-and-capitals.txt') as file:
         return [line.strip() for line in file]
+
+
+def choose_difficulty():
+    print("Select a difficulty level:")
+    print("1. Easy (guess a country)")
+    print("2. Medium (guess a capital)")
+    print("3. Hard (guess both a country and its capital)")
+    while True:
+        selection = input("Selecte a difficulty level (1-3): ")
+        if selection == '1':
+            return 'easy'
+        elif selection == '2':
+            return 'medium'
+        elif selection == '3':
+            return 'hard'
+        print("Invalid selection. Please enter a number between 1 and 3.")
+
+
+def get_secret(difficulty):
+    words = get_words()
+    word_index = random.randint(0, len(words) - 1)
+    country, city = words[word_index].split(' | ')
+    secret = {
+        "easy": country,
+        "medium": city,
+        "hard": country + city
+    }
+    return secret[difficulty], words[word_index]
 
 
 def is_valid_guess(guess, guesses):
@@ -90,18 +95,18 @@ def is_game_over(display, lives):
 
 def display_game_state(display, misses, guesses, lives):
     clear_screen()
-    print(stages[min(misses, len(stages) - 1)])
+    print(ASCII_ART[min(misses, len(ASCII_ART) - 1)])
     print(' '.join(display))
     display_guesses(guesses)
     display_lives(lives)
 
 
 def play_game(game):
-    lives, word = game
+    lives, (word, answer) = game
     display = ['_'] * len(word)
     guesses = set()
     clear_screen()
-    print(stages[0])
+    print(ASCII_ART[0])
     print("Secret: " + ' '.join(display))
     misses = 0
     running = True
@@ -118,22 +123,21 @@ def play_game(game):
         display, lives, guesses, misses = process_guess(guess, word, display, lives, guesses, misses)
         if is_game_over(display, lives):
             running = False
+            clear_screen()
             if '_' not in display:
                 print("You win!")
-                print(stages[0])
+                print(ASCII_ART[0])
             else:
                 print("You lose!")
-                print(stages[-1])
+                print(ASCII_ART[-1])
+            print(f"The secret word was {word} for the country and city: {answer}")
 
 
-def main():
+# Main program
+if __name__ == '__main__':
     display_title_screen()
     input('Press any key to continue...')
     clear_screen()
     difficulty = choose_difficulty()
     game = get_game(difficulty)
     play_game(game)
-
-
-if __name__ == '__main__':
-    main()
